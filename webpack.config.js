60e5c9ee1a1bef4c4a2f2a1de9786e426533c4c2
Justin <jniestroy@gmail.com>
2024-09-05 15:00:29 -0400

    env changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,64 @@
 const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
+const fs = require("fs");
 
-// call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
+module.exports = (env, argv) => {
+  // Get the root path (assuming your webpack config is in the root of your project!)
+  const currentPath = path.join(__dirname);
 
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+  // Create the fallback path (the production .env)
+  const basePath = currentPath + "/.env";
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  target: "electron-renderer",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+  // We're concatenating the environment name to our filename to specify the correct env file!
+  const envPath = basePath + "." + env.ENVIRONMENT;
+
+  // Check if the file exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+  // Set the path parameter in the dotenv config
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  // reduce it to a nice object, the same as before (but with the variables from the file!)
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: env.ENVIRONMENT,
+    entry: "./src/index.js",
+    target: "electron-renderer",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          use: ["file-loader"],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-  },
-  plugins: [new webpack.DefinePlugin(envKeys)],
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  };
 };
