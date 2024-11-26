5105f89fa626df58856d99b98da367f9e31cd1fd
Justin <jniestroy@gmail.com>
2024-11-26 13:14:18 -0500

    prod build script

---
+++ build-prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VITE_FAIRSCAPE_API_URL=http://localhost:8080/api \
+VITE_FAIRSCAPE_FE_URL=http://localhost:5173/ \
+npm run build
+
+# Generate date in YYYY-MM-DD format
+DATE=$(date '+%Y-%m-%d')
+
+# Build the Docker image with production environment variables
+docker build \
+  --build-arg VITE_FAIRSCAPE_API_URL=${PROD_API_URL} \
+  --build-arg VITE_FAIRSCAPE_FE_URL=${PROD_FE_URL} \
+  -f Dockerfile \
+  -t ghcr.io/fairscape/fairscapefrontend:RELEASE.${DATE} .
+
+
+docker push ghcr.io/fairscape/fairscapefrontend:RELEASE.${DATE}
