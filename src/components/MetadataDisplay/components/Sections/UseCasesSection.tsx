a0c40ebd44a8a7ff3f1a5e2db46628f9e3513a25
jniestroy <jniestroy@gmail.com>
2025-08-29 10:45:36 -0400

    missed import

--- src/components/MetadataDisplay/components/Sections/UseCasesSection.tsx
+++ src/components/MetadataDisplay/components/Sections/UseCasesSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { UseCasesData } from "../../utils/metadataProcessing";
 import MetadataField from "../Fields/MetadataField";
 
